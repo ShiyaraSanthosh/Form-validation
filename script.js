ad7b9d2cb4940d3667a55f2dcaf67677c4a119d1
ShiyaraSanthosh <shiyarasanthosh3@gmail.com>
2025-07-02 02:41:22 +0530

    form

--- script.js
+++ script.js
@@ -1,60 +1,93 @@
 // form
-function validform() {
-    const username= document.getElementById("username").value;
-    const email = document.getElementById("email").value;       
-    const password = document.getElementById("password").value;
-    const higherEducation = document.getElementById("higherEducation").value;
-    const phoneno = document.getElementById("phoneno").value;
-    const description = document.getElementById("description").value;
-    if(username==='' || email==='' || password==='' || higherEducation==='' || phoneno==='' || description==='')
-    {
-        alert("you can't sumbit form without filling all the fields");
-        return false;
+function validform(event) {
+    event.preventDefault();
+    const username = document.getElementById("username");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    const higherEducation = document.getElementById("higherEducation");
+    const phoneno = document.getElementById("phoneno");
+    const description = document.getElementById("description");
+  
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const higherEducationValue = higherEducation.value;
+    const phonenoValue = phoneno.value;
+    const descriptionValue = description.value;
+//username
+    const usernameError = document.getElementById("usernameError");
+    if (usernameValue === ''|| usernameValue === null){
+        usernameError.innerHTML = "username is required";
+        usernameError.style.color = "red";
     }
-    if(username===''){
-       alert("Username cannot be empty");
-       return false;
-    }
-    const usernameRegex= /^[a-zA-Z0-9]{3,}$/;
-    if(!usernameRegex.test(username)){
-        username.parentElement.textcontent ='<i class="fa fa-check-circle" aria-hidden="true" ></i>';
-        username.style.color = "#63E6BE";
+    const usernameRegex = /^[a-zA-Z0-9]{3,}$/;
+    if(!usernameRegex.test(usernameValue)){
+        usernameError.innerHTML = "valid username";
+        
+        usernameError.style.color = "green";
+        let icon = document.createElement("i");
+        icon.className = "fa fa-check-circle validation-icon w-full absolute top-[35px] ";
+        icon.style.color = "#63E6BE";
+        icon.style.marginLeft = "300px";
+        username.parentElement.appendChild(icon);
+      
         return true;
+        
     } else {
-        username.parentElement.innerHTML = '<i class="fa-solid fa-circle-xmark" style="color: #ff0000;"></i>';
-        return false;
-    }
-    if(email===''){
-        alert("Email cannot be empty");
+        usernameError.innerHTML = "Invalid username";
+        
+        usernameError.style.color = "red";
+        let icon = document.createElement("i");
+        icon.className ="fa-solid fa-circle-xmark";
+        icon. style.color = "red";
+        icon.style.marginLeft = "5px";
+        username.parentElement.appendChild(icon);
         return false;
     }
 
+  //email 
+   const emailError = document.getElementById("emailError");
+   if (emailValue ===''|| emailValue=== null){
+         emailError.innerHTML = "Email is required";
+         emailError.style.color = "red";
+   }
+   const emailRegex = "[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$";
+   if (emailValue.test(emailRegex)){
+    emailError.innerHTML = "valid email";
+    emailError.style.color = "green";
+    let icon = document.createElement("i");
+    icon.className = "fa fa-check-circle validation-icon w-full absolute top-[35px] ";
+    icon.style.color = "#63E6BE";
+    icon.style.marginLeft = "300px";
+    email.parentElement.appendChild(icon);
+    return true;
+    
+
+
+   } else {
+    emailError.innerHTML = "Invalid email";
+    emailError.style.color = "red";
+    let icon = document.createElement("i");
+    icon.className = "fa-solid fa-circle-xmark";
+    icon.style.color = "red";
+    icon.style.marginLeft = "5px";
+    email.parentElement.appendChild(icon);
+    return false;
+   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-//character count
-const charcount = document.getElementById("char-count");
-const description = document.getElementById("description");
-const maxLength = 150;
-description.addEventListener("input",function(){
+  
+  //character count
+  const charcount = document.getElementById("char-count");
+  const description = document.getElementById("description");
+  const maxLength = 150;
+  description.addEventListener("input", function () {
     const inputLength = description.value.length;
     charcount.innerHTML = inputLength;
     if (inputLength >= maxLength) {
-        inputField.style.backgroundColor = "#e74c3c";
-        inputField.style.color = "#ffffff";
-      } else {
-        inputField.style.backgroundColor = "";
-        inputField.style.color = "";
-      }
-
-});
+      inputField.style.backgroundColor = "#e74c3c";
+      inputField.style.color = "#ffffff";
+    } else {
+      inputField.style.backgroundColor = "";
+      inputField.style.color = "";
+    }
+  });
